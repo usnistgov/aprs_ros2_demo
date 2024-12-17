bd7b856ddb4758fb0ac2c48295ac7a6d40a29b0d
Justin Albrecht <jalbrecht23@gmail.com>
2024-12-17 15:23:25 -0500

    modified robot status frame

--- akb_application/akb_application/robot_status_frame.py
+++ akb_application/akb_application/robot_status_frame.py
@@ -1,71 +1,140 @@
-import customtkinter as ctk
+from typing import Optional
 
+import customtkinter as ctk
 from tkinter import ttk
+
 from rclpy.node import Node
+from rclpy.time import Time, Duration
+
 from std_msgs.msg import Bool
 
 
-class RobotStatusFrame(ctk.CTkFrame):
-    def __init__(self, frame, node: Node):
-        super().__init__(frame, height=300, width=600, fg_color="#EBEBEB")
-        self.rowconfigure(0, weight=1)
-        self.columnconfigure((0,1,2), weight=1)
+class StatusFrame(ctk.CTkFrame):
+    def __init__(self, frame, node: Node, row: int, col: int):
+        super().__init__(frame, fg_color="transparent")
+        
+        self.grid_columnconfigure((0, 2), weight=1)
+        self.grid(row=row, column=col, sticky="SE", padx=20, pady=20)
 
+        font = ctk.CTkFont("Roboto", 18, weight="bold")
+
+        title = ctk.CTkLabel(self, text="Robot Status", font=font)
+
+        title.grid(row=0, column=0, columnspan=3)
+
+        sep = ttk.Separator(self, orient='horizontal')
+        sep.grid(row=1, column=0, columnspan=3, sticky="WE")
+        
         self.node = node
 
-        self.fanuc_status_frame = RobotStatusSubFrame(self, self.node, "fanuc")
-        self.fanuc_status_frame.grid(row=0, column=0, padx=2)
+        self.fanuc_status_frame = RobotStatusFrame(self, self.node, "fanuc")
+        self.fanuc_status_frame.grid(row=2, column=0, sticky="WE")
 
         sep = ttk.Separator(self, orient='vertical')
-        sep.grid(row=0, column=1, sticky="ns")
+        sep.grid(row=2, column=1)
 
-        self.motoman_status_frame = RobotStatusSubFrame(self, self.node, "motoman")
-        self.motoman_status_frame.grid(row=0, column=2, padx=2)
+        self.motoman_status_frame = RobotStatusFrame(self, self.node, "motoman")
+        self.motoman_status_frame.grid(row=2, column=2, sticky="WE")
 
-class RobotStatusSubFrame(ctk.CTkFrame):
+
+class RobotStatusFrame(ctk.CTkFrame):
     def __init__(self, frame, node: Node, robot_name: str):
-        super().__init__(frame, height=300, width=290, fg_color="#EBEBEB")
+        super().__init__(frame, fg_color="transparent")
         self.node = node
 
-        self.rowconfigure((0,1,2,3), weight=1)
-        self.columnconfigure((0,1), weight=1)
+        # Layout
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure((2, 3, 4), weight=1)
 
-        self.joint_state_broadcaster_active = False
-        self.joint_trajectory_controller_active = False
-        self.pneumatic_gripper_active = False
+        # Variables for controller state
+        self.state_status = ctk.BooleanVar(value=False)
+        self.command_status = ctk.BooleanVar(value=False)
+        self.gripper_status = ctk.BooleanVar(value=False)
 
-        self.joint_state_broadcaster_sub = self.node.create_subscription(Bool, f"{robot_name}/joint_state_broadcaster_status", self.joint_state_broadcaster_cb, 1)
-        self.joint_trajectory_controller_sub = self.node.create_subscription(Bool, f"{robot_name}/joint_trajectory_controller_status", self.joint_trajectory_controller_cb, 1)
-        self.pneumatic_gripper_sub = self.node.create_subscription(Bool, f"{robot_name}/pneumatic_gripper_status", self.pneumatic_gripper_status_cb, 1)
+        # Timing
+        self.last_update_time: dict[str, Optional[Time]] = {s: None for s in ['state', 'command', 'gripper']}
 
-        ctk.CTkLabel(self, text= f"{robot_name.capitalize()} controllers:", height=15).grid(row=0, column=0, columnspan=2, pady=10)
+        # ROS Subscriptions
+        topic_names = {
+            "state": f"{robot_name}/joint_state_broadcaster_status",
+            "command": f"{robot_name}/joint_trajectory_controller_status",
+            "gripper": f"{robot_name}/pneumatic_gripper_status"
+        }
 
-        self.joint_state_broadcaster_label = ctk.CTkLabel(self, text="Joint State Broadcaster:", height=15)
-        self.joint_state_broadcaster_label.grid(row=1, column=0, pady=2, padx=2)
-        self.joint_state_broadcaster_status_label = ctk.CTkLabel(self, text="INACTIVE", text_color="red", height=15)
-        self.joint_state_broadcaster_status_label.grid(row=1, column=1, pady=2, padx=2)
+        self.node.create_subscription(Bool, topic_names["state"], self.state_cb, 1)
+        self.node.create_subscription(Bool, topic_names["command"], self.command_cb, 1)
+        self.node.create_subscription(Bool, topic_names["gripper"], self.gripper_cb, 1)
 
-        self.joint_trajectory_controller_label = ctk.CTkLabel(self, text="Joint Trajectory Controller:", height=15)
-        self.joint_trajectory_controller_label.grid(row=2, column=0, pady=2, padx=2)
-        self.joint_trajectory_controller_status_label = ctk.CTkLabel(self, text="INACTIVE", text_color="red", height=15)
-        self.joint_trajectory_controller_status_label.grid(row=2, column=1, pady=2, padx=2)
+        # Widgets
+        font = ctk.CTkFont("Roboto", 16, weight="bold")
 
-        self.pneumatic_gripper_label = ctk.CTkLabel(self, text="Pneumatic Gripper:", height=15)
-        self.pneumatic_gripper_label.grid(row=3, column=0, pady=2, padx=2)
-        self.pneumatic_gripper_status_label = ctk.CTkLabel(self, text="INACTIVE", text_color="red", height=15)
-        self.pneumatic_gripper_status_label.grid(row=3, column=1, pady=2, padx=2)
+        robot_name_label = ctk.CTkLabel(self, text=f"{robot_name.capitalize()}:", font=font)
+        robot_name_label.grid(row=0, column=0)
 
-    def joint_state_broadcaster_cb(self, msg: Bool):
-        if self.joint_state_broadcaster_active != msg.data:
-            self.joint_state_broadcaster_active = msg.data
-            self.joint_state_broadcaster_status_label.configure(text=("ACTIVE" if self.joint_state_broadcaster_active else "INACTIVE"), text_color=("green" if self.joint_state_broadcaster_active else "red"))
+        bar = ttk.Separator(self, orient="horizontal")
+        bar.grid(row=1, column=0, sticky="WE", pady=2, padx=5)
+
+        ControllerStatus(self, "State", self.state_status, row=2)
+        ControllerStatus(self, "Command", self.command_status, row=3)
+        ControllerStatus(self, "Gripper", self.gripper_status, row=4)
+
+        self.update()
         
-    def joint_trajectory_controller_cb(self, msg: Bool):
-        if self.joint_trajectory_controller_active != msg.data:
-            self.joint_trajectory_controller_active = msg.data
-            self.joint_trajectory_controller_status_label.configure(text=("ACTIVE" if self.joint_trajectory_controller_active else "INACTIVE"), text_color=("green" if self.joint_trajectory_controller_active else "red"))
+    def state_cb(self, msg: Bool):
+        self.last_update_time['state'] = self.node.get_clock().now()
+        self.state_status.set(msg.data)
         
-    def pneumatic_gripper_status_cb(self, msg: Bool):
-        if self.pneumatic_gripper_active != msg.data:
-            self.pneumatic_gripper_active = msg.data
-            self.pneumatic_gripper_status_label.configure(text=("ACTIVE" if self.pneumatic_gripper_active else "INACTIVE"), text_color=("green" if self.pneumatic_gripper_active else "red"))
+    def command_cb(self, msg: Bool):
+        self.last_update_time['command'] = self.node.get_clock().now()
+        self.command_status.set(msg.data)
+        
+    def gripper_cb(self, msg: Bool):
+        self.last_update_time['gripper'] = self.node.get_clock().now()
+        self.gripper_status.set(msg.data)
+
+    def update(self, *args):
+        # Reset status to INACTIVE if topic is not published for 3 seconds
+        for key, time in self.last_update_time.items():
+            if time is None:
+                continue
+
+            if (self.node.get_clock().now() - time).nanoseconds > 3E9:
+                if key == 'state':
+                    self.state_status.set(False)
+                elif key == 'command':
+                    self.command_status.set(False)
+                elif key == 'gripper':
+                    self.gripper_status.set(False)
+
+        self.after(100, self.update)
+        
+
+class ControllerStatus(ctk.CTkFrame):
+    def __init__(self, frame, controller_name: str, status: ctk.BooleanVar, row):
+        super().__init__(frame, fg_color="transparent")
+
+        # Add trace to status variable
+        self.status = status
+        self.status.trace_add('write', self.update_label)
+
+        # Layout
+        self.grid_columnconfigure((0,1), weight=1)
+        self.grid(row=row, column=0, padx=10, pady=2, sticky="WE")
+
+        # Labels
+        name_font = ctk.CTkFont("Roboto", 16)
+        status_font = ctk.CTkFont("Roboto", 14, weight="bold")
+
+        controller_name_label = ctk.CTkLabel(self, text=f"{controller_name}:", font=name_font, width=80)
+        self.status_label = ctk.CTkLabel(self, text="INACTIVE", text_color="white", fg_color="red", font=status_font, width=80, corner_radius=5)
+        
+        # Add widgets to frame
+        controller_name_label.grid(row=0, column=0,padx=5, sticky="WE")
+        self.status_label.grid(row=0, column=1, padx=5, sticky="E")
+        
+    def update_label(self, *args):
+        if self.status.get():
+            self.status_label.configure(text="ACTIVE", fg_color="green")
+        else:
+            self.status_label.configure(text="INACTIVE", fg_color="red")
+        
